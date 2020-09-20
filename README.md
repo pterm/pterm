164dbfe1593acc8d602536735f8fe4fc6eebf825
MarvinJWendt <git@marvinjwendt.com>
2020-09-20 02:58:58 +0200

    feat: integrate ci

--- README.md
+++ README.md
@@ -38,3 +38,87 @@
 </a>
 
 ---
+
+## Examples
+
+<!-- examples:start -->
+### demo
+
+![Animation](https://raw.githubusercontent.com/pterm/pterm/master/_examples/demo/animation.svg)
+
+```go
+package main
+
+import (
+	"time"
+
+	"github.com/pterm/pterm"
+)
+
+func main() {
+
+	for _, p := range pterm.AllPrinters {
+		p.Println("This is the default ", p.Prefix.Text, " printer.")
+		p.WithScope("scope").Println("This is the ", p.Prefix.Text, " printer with a scope.")
+		p.WithScope("custom", pterm.New(pterm.FgLightMagenta, pterm.Bold, pterm.BgWhite)).Println("This is the ", p.Prefix.Text, " printer with a custom scope style.")
+		time.Sleep(time.Second)
+		pterm.Println()
+	}
+
+	customPrefixPrinter := pterm.PrefixPrinter{
+		Prefix: pterm.Prefix{
+			Text:  "CUSTOM",
+			Style: []pterm.Color{pterm.FgLightRed, pterm.BgBlue},
+		},
+	}
+
+	customPrefixPrinter.Println("This is a custom PrefixPrinter :)")
+
+}
+
+```
+
+### override-default-printer
+
+![Animation](https://raw.githubusercontent.com/pterm/pterm/master/_examples/override-default-printer/animation.svg)
+
+```go
+package main
+
+import "github.com/pterm/pterm"
+
+func main() {
+
+	pterm.ErrorPrinter.Println("This is the default ErrorPrinter")
+
+	pterm.ErrorPrinter.Prefix = pterm.Prefix{
+		Text:  "OVERRIDE",
+		Style: pterm.Style{pterm.BgCyan, pterm.FgRed},
+	}
+
+	pterm.ErrorPrinter.Println("This is the default ErrorPrinter after the prefix was overridden")
+}
+
+```
+
+### print-with-color
+
+![Animation](https://raw.githubusercontent.com/pterm/pterm/master/_examples/print-with-color/animation.svg)
+
+```go
+package main
+
+import "github.com/pterm/pterm"
+
+func main() {
+	// Simple Println with different colored words.
+	pterm.Println(pterm.Red("Hello, ") + pterm.Green("World") + pterm.Cyan("!"))
+}
+
+```
+
+<!-- examples:end -->
+
+
+
+
---
+++ ci/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+)
+
+func main() {
+	fmt.Println("## Generating Examples")
+	files, err := ioutil.ReadDir("./_examples/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var readmeExamples string
+
+	for _, f := range files {
+		fmt.Println("### Generating animations for example '" + f.Name() + "'")
+		animationDataPath := "./_examples/" + f.Name() + "/animation_data.json"
+		animationSvgPath := "./_examples/" + f.Name() + "/animation.svg"
+
+		if fileExists(animationDataPath) {
+			fmt.Println("#### animation_data.json already exists. Removing it.")
+			err := os.Remove(animationDataPath)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		if fileExists(animationDataPath) {
+			fmt.Println("#### animation.svg already exists. Removing it.")
+			err := os.Remove(animationSvgPath)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		fmt.Println("#### Running asciinema")
+		execute(`asciinema rec ` + animationDataPath + ` -c "go run ./_examples/` + f.Name() + `"`)
+
+		fmt.Println("#### Adding sleep to end of animation_data.json")
+		sleepString := `[5, "o", "\r\nrestarting...\r\n"]`
+		animationDataFile, err := os.OpenFile(animationDataPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+		if err != nil {
+			log.Println(err)
+		}
+		defer animationDataFile.Close()
+		if _, err := animationDataFile.WriteString(sleepString); err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println("#### Generating SVG")
+		execute(`svg-term --in ` + animationDataPath + ` --out ` + animationSvgPath + ` --window true --no-optimize`)
+
+		fmt.Println("#### Generating README.md")
+		readmeString := "# " + f.Name() + "\n\n![Animation](animation.svg)"
+		err = ioutil.WriteFile("./_examples/"+f.Name()+"/README.md", []byte(readmeString), 0600)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("#### Adding example to global example list")
+
+		exampleCode, err := ioutil.ReadFile("./_examples/" + f.Name() + "/main.go")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		readmeExamples += "### " + f.Name() + "\n\n"
+		readmeExamples += "![Animation](https://raw.githubusercontent.com/pterm/pterm/master/_examples/" + f.Name() + "/animation.svg)\n\n"
+		readmeExamples += "```go\n"
+		readmeExamples += string(exampleCode) + "\n"
+		readmeExamples += "```"
+		readmeExamples += "\n\n"
+
+		fmt.Println("#### Cleaning files")
+		os.Remove(animationDataPath)
+
+	}
+
+	fmt.Println("### Appending examples to root README.md")
+
+	readmeContent, err := ioutil.ReadFile("./README.md")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	beforeRegex := regexp.MustCompile(`(?ms).*<!-- examples:start -->`)
+	afterRegex := regexp.MustCompile(`(?ms)<!-- examples:end -->.*`)
+
+	before := beforeRegex.FindAllString(string(readmeContent), 1)[0]
+	after := afterRegex.FindAllString(string(readmeContent), 1)[0]
+
+	var newReadmeContent string
+
+	newReadmeContent += before + "\n"
+	newReadmeContent += readmeExamples
+	newReadmeContent += after + "\n"
+
+	err = ioutil.WriteFile("./README.md", []byte(newReadmeContent), 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func execute(command string) {
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	fmt.Println("Running: " + cmd.String())
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
