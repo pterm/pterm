3c2395661df6df79c564f60d6334f10daf71293a
MarvinJWendt <git@marvinjwendt.com>
2020-09-21 14:03:11 +0200

    ci: add release system

--- pterm.go
+++ pterm.go
@@ -1,4 +1,4 @@
 package pterm
 
 // PTERM_VERSION returns the current version of PTerm in SemVer format.
-const PTERM_VERSION = "v0.0.0"
+const PTERM_VERSION = "v0.0.0" // <---VERSION---> | This comment is for our CI System. Do not edit it.
