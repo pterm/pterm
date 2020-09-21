14ebcd3512e202d428ba4ad25234be48be91e741
MarvinJWendt <git@marvinjwendt.com>
2020-09-21 13:50:58 +0200

    docs: update demo

--- _examples/demo/main.go
+++ _examples/demo/main.go
@@ -8,16 +8,35 @@ import (
 
 func main() {
 
-	twoSeconds := time.Tick(time.Second * 2)
-	// fiveSeconds := time.Tick(time.Second*5)
-	// tenSeconds := time.Tick(time.Second*10)
-	// second := time.Tick(time.Second)
-	// halfSecond := time.Tick(time.Second/2)
-	// quarterSecond := time.Tick(time.Second/4)
-
 	pterm.PrintHeader("You can do many things with PTerm")
 
-	<-twoSeconds
+	time.Sleep(time.Second * 3)
+
+	pterm.Println(pterm.Cyan("Like this header above!"))
+
+	time.Sleep(time.Second * 2)
+
+	clear()
+
+	pterm.PrintSuccess("You can print success messages!")
+
+	time.Sleep(time.Second * 2)
+
+	pterm.PrintInfo("Or infos!")
+
+	time.Sleep(time.Second)
+
+	pterm.PrintError("Or errors!")
+
+	time.Sleep(time.Second)
+
+	pterm.PrintWarning("Or warnings!")
+
+	time.Sleep(time.Second)
+
+	pterm.PrintDescription("Even descriptions can be printed...")
+
+	time.Sleep(time.Second * 2)
 
 	customPrefixPrinter := pterm.PrefixPrinter{
 		Prefix: pterm.Prefix{
@@ -26,6 +45,63 @@ func main() {
 		},
 	}
 
-	customPrefixPrinter.Println("This is a custom PrefixPrinter :)")
+	customPrefixPrinter.Println("Or a custom PrefixPrinter can be crafted :)")
 
+	time.Sleep(time.Second * 2)
+
+	pterm.WarningPrinter.WithScope("custom-scope").Println("PrefixPrinters also support scopes!")
+
+	time.Sleep(time.Second * 4)
+
+	clear()
+
+	pterm.PrintHeader("Everything can be customized!")
+
+	time.Sleep(time.Second * 2)
+
+	headerStyles := []pterm.Style{
+		{pterm.BgGreen},
+		{pterm.BgWhite},
+		{pterm.BgRed},
+		{pterm.BgBlue},
+		{pterm.BgYellow},
+		{pterm.BgLightMagenta},
+	}
+
+	for _, style := range headerStyles {
+		clear()
+		pterm.HeaderPrinter{Header: pterm.Header{
+			BackgroundStyle: style,
+			TextStyle:       pterm.Style{pterm.FgLightWhite},
+			Margin:          5,
+		}}.Println("Everything can be customized")
+		time.Sleep(time.Second / 2)
+	}
+
+	for i := 0; i < 10; i++ {
+		clear()
+		style := headerStyles[len(headerStyles)-1]
+		pterm.HeaderPrinter{Header: pterm.Header{
+			BackgroundStyle: style,
+			TextStyle:       pterm.Style{pterm.FgLightWhite},
+			Margin:          5 + i,
+		}}.Println("Everything can be customized")
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	for i := 0; i < 15; i++ {
+		clear()
+		style := headerStyles[len(headerStyles)-1]
+		pterm.HeaderPrinter{Header: pterm.Header{
+			BackgroundStyle: style,
+			TextStyle:       pterm.Style{pterm.FgLightWhite},
+			Margin:          15 - i,
+		}}.Println("Everything can be customized")
+		time.Sleep(time.Millisecond * 100)
+	}
+
+}
+
+func clear() {
+	print("\033[H\033[2J")
 }
