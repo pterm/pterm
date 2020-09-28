a7d70975575b786051fae0b414ca9ae4c17fa199
MarvinJWendt <git@marvinjwendt.com>
2020-09-28 12:41:39 +0200

    refactor: better comments for `Printo`

--- print.go
+++ print.go
@@ -6,12 +6,14 @@ import (
 )
 
 // Printo overrides the current line in a terminal.
+// If the current line is empty, the text will be printed like with pterm.Print.
+// To create a new line, which
 // Example:
-// pterm.Print("Hello, World")
+// pterm.Print("Hello, World") // You could also use pterm.Printo here
 // time.Sleep(time.Second)
 // pterm.Oprint("Hello, Earth!")
 func Printo(a ...interface{}) {
-	Fprint(os.Stdout, "\r", Sprint(a...))
+	Fprint(os.Stdout, "\r"+Sprint(a...))
 }
 
 // Fprinto prints Printo to a custom writer.
