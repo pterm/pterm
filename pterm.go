874a217a1fc54fc18dd7d1aac5ea0b4c65564b9e
MarvinJWendt <git@marvinjwendt.com>
2020-09-30 15:56:59 +0200

    refactor: bump version to "v0.2.0"

--- pterm.go
+++ pterm.go
@@ -1,4 +1,4 @@
 package pterm
 
 // Version returns the current version of PTerm in SemVer format.
-const Version = "v0.1.0" // <---VERSION---> | This comment is for our CI System. Do not edit it.
+const Version = "v0.2.0" // <---VERSION---> | This comment is for our CI System. Do not edit it.
