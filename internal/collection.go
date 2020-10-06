b75f43468519d6585d8861b53e6fa3b50257ad71
MarvinJWendt <git@marvinjwendt.com>
2020-10-06 22:56:58 +0200

    style: format project

--- internal/collection.go
+++ internal/collection.go
@@ -1,5 +1,7 @@
 package internal
 
 // RandomStrings contains a list of random strings to use while testing.
-var RandomStrings = []string{"hello world", "²³14234!`§=)$-.€@_&", "This is a sentence.",
-	"This\nstring\nhas\nmultiple\nlines", "windows\r\nline\r\nendings", "\rtext"}
+var RandomStrings = []string{
+	"hello world", "²³14234!`§=)$-.€@_&", "This is a sentence.", "This\nstring\nhas\nmultiple\nlines",
+	"windows\r\nline\r\nendings", "\rtext",
+}
