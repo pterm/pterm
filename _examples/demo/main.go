e2b7c8bac81760153a37ac084c0fd4ed5cd63c1a
MarvinJWendt <git@marvinjwendt.com>
2020-10-07 16:29:51 +0200

    docs(examples): fix examples

--- _examples/demo/main.go
+++ _examples/demo/main.go
@@ -18,7 +18,8 @@ func main() {
 	// Useful when debugging.
 	const second = time.Second
 
-	pterm.DefaultHeader.WithBackgroundStyle(pterm.BgLightBlue).WithMargin(10).Println("PTDP - PTerm Demo Program")
+	pterm.DefaultHeader.WithBackgroundStyle(pterm.NewStyle(pterm.BgLightBlue)).WithMargin(10).Println(
+		"PTDP - PTerm Demo Program")
 	pterm.Info.Println("This animation was generated with the latest version of PTerm!" +
 		"\nPTerm works on nearly every terminal and operating system." +
 		"\nIt's super easy to use!" +
@@ -42,7 +43,8 @@ func main() {
 
 	clear()
 
-	pterm.DefaultHeader.WithBackgroundStyle(pterm.BgLightBlue).WithMargin(10).Println("Pseudo Application created with PTerm")
+	pterm.DefaultHeader.WithBackgroundStyle(pterm.NewStyle(pterm.BgLightBlue)).WithMargin(10).Println(
+		"Pseudo Application created with PTerm")
 
 	time.Sleep(second)
 
--- _examples/header-custom/main.go
+++ _examples/header-custom/main.go
@@ -7,10 +7,10 @@ func main() {
 
 	// Build on top of DefaultHeader
 	pterm.DefaultHeader. // Use DefaultHeader as base
-				WithMargin(15).                     // Set Margin to 15
-				WithBackgroundStyle(pterm.BgCyan).  // Set BackgroundStyle to Cyan
-				WithTextStyle(pterm.FgBlack).       // Set TextStyle to Black
-				Println("This is a custom header!") // Print header
+				WithMargin(15).                                    // Set Margin to 15
+				WithBackgroundStyle(pterm.NewStyle(pterm.BgCyan)). // Set BackgroundStyle to Cyan
+				WithTextStyle(pterm.NewStyle(pterm.FgBlack)).      // Set TextStyle to Black
+				Println("This is a custom header!")                // Print header
 	// Instead of printing the header you can set it to a variable.
 	// You can then reuse your custom header.
 
