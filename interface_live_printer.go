b41b59c10d1326db9fbc0abd12a5b8625f30a1d5
MarvinJWendt <git@marvinjwendt.com>
2020-10-08 11:00:57 +0200

    feat: add `LivePrinter` interface

---
+++ interface_live_printer.go
@@ -0,0 +1,11 @@
+package pterm
+
+// LivePrinter is a printer which can update it's output live.
+type LivePrinter interface {
+	// GenericStart runs Start, but returns a LivePrinter.
+	// This is used for the interface LivePrinter.
+	// You most likely want to use Start instead of this in your program.
+	GenericStart() LivePrinter
+
+	GenericStop() LivePrinter
+}
