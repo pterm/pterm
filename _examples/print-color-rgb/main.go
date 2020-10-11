4a55bb752d22c895038698984eea24a4814bebe6
MarvinJWendt <git@marvinjwendt.com>
2020-10-11 23:13:49 +0200

    feat: implement `TextPrinter` into `RGB`
    Closes: #36

---
+++ _examples/print-color-rgb/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/pterm/pterm"
+
+func main() {
+	pterm.NewRGB(178, 44, 199).Println("This text is printed with a custom RGB!")
+	pterm.NewRGB(15, 199, 209).Println("This text is printed with a custom RGB!")
+	pterm.NewRGB(201, 144, 30).Println("This text is printed with a custom RGB!")
+}
--- rgb.go
+++ rgb.go
@@ -1,6 +1,9 @@
 package pterm
 
-import "github.com/pterm/pterm/internal"
+import (
+	"github.com/gookit/color"
+	"github.com/pterm/pterm/internal"
+)
 
 // RGB color model is an additive color model in which red, green, and blue light are added together in various ways to reproduce a broad array of colors.
 // The name of the model comes from the initials of the three additive primary colors, red, green, and blue.
@@ -28,4 +31,44 @@ func (rgb RGB) Fade(min, max, current float32, end RGB) RGB {
 		G: uint8(internal.MapRangeToRange(min, max, float32(rgb.G), float32(end.G), current)),
 		B: uint8(internal.MapRangeToRange(min, max, float32(rgb.B), float32(end.B), current)),
 	}
+}
+
+// Sprint formats using the default formats for its operands and returns the resulting string.
+// Spaces are added between operands when neither is a string.
+func (rgb RGB) Sprint(a ...interface{}) string {
+	return color.RGB(rgb.R, rgb.G, rgb.B).Sprint(a...)
+}
+
+// Sprintln formats using the default formats for its operands and returns the resulting string.
+// Spaces are always added between operands and a newline is appended.
+func (rgb RGB) Sprintln(a ...interface{}) string {
+	return Sprintln(rgb.Sprint(a...))
+}
+
+// Sprintf formats according to a format specifier and returns the resulting string.
+func (rgb RGB) Sprintf(format string, a ...interface{}) string {
+	return rgb.Sprint(Sprintf(format, a...))
+}
+
+// Print formats using the default formats for its operands and writes to standard output.
+// Spaces are added between operands when neither is a string.
+// It returns the number of bytes written and any write error encountered.
+func (rgb RGB) Print(a ...interface{}) TextPrinter {
+	Print(rgb.Sprint(a...))
+	return &rgb
+}
+
+// Println formats using the default formats for its operands and writes to standard output.
+// Spaces are always added between operands and a newline is appended.
+// It returns the number of bytes written and any write error encountered.
+func (rgb RGB) Println(a ...interface{}) TextPrinter {
+	Println(rgb.Sprint(a...))
+	return &rgb
+}
+
+// Printf formats according to a format specifier and writes to standard output.
+// It returns the number of bytes written and any write error encountered.
+func (rgb RGB) Printf(format string, a ...interface{}) TextPrinter {
+	Print(rgb.Sprintf(format, a...))
+	return &rgb
 }
