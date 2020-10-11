b5dba010b45d4d4b31481f4f3474a8592eefb1aa
floaust <flo.extra.email@gmail.com>
2020-10-11 22:24:03 +0200

    feat: add color fade function and `RBG`

--- internal/map_range_to_range_test.go
+++ internal/map_range_to_range_test.go
@@ -8,4 +8,6 @@ import (
 func TestMapRangeToRange(t *testing.T) {
 	assert.Equal(t, 127, MapRangeToRange(0, 100, 0, 255, 50))
 	assert.Equal(t, 127, MapRangeToRange(0, 400, 0, 255, 200))
+	assert.Equal(t, 127, MapRangeToRange(-200, 200, 0, 255, 0))
+	assert.Equal(t, 127, MapRangeToRange(0, 200.123, 0, 254.3, 100))
 }
