f02819d62de6548233b93c4dac91e1a531f4ccfb
floaust <flo.extra.email@gmail.com>
2020-10-11 22:16:11 +0200

    feat: add color fade function and `RBG`

--- rgb.go
+++ rgb.go
@@ -2,16 +2,21 @@ package pterm
 
 import "github.com/pterm/pterm/internal"
 
+// RGB color model is an additive color model in which red, green, and blue light are added together in various ways to reproduce a broad array of colors.
+// The name of the model comes from the initials of the three additive primary colors, red, green, and blue.
+// https://en.wikipedia.org/wiki/RGB_color_model
 type RGB struct {
 	R uint8
 	G uint8
 	B uint8
 }
 
+// NewRGB returns a new RGB.
 func NewRGB(r, g, b uint8) RGB {
 	return RGB{R: r, G: g, B: b}
 }
 
+// Fade fades one RGB value to another RGB value, by giving the function a minimum, maximum and current value.
 func (rgb RGB) Fade(min, max, current float32, end RGB) RGB {
 	return RGB{
 		R: uint8(internal.MapRangeToRange(min, max, float32(rgb.R), float32(end.R), current)),
