08e15f94968bb036bd2d8343382ca20830bc6982
floaust <flo.extra.email@gmail.com>
2020-10-13 01:24:28 +0200

    feat: add `list_printer` body

---
+++ _examples/list/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"time"
+
+	"github.com/pterm/pterm"
+)
+
+func main() {
+
+	var is []pterm.ListItem
+
+	is = append(is, pterm.DefaultListItem.WithLevel(0).WithText("0"))
+	is = append(is, pterm.DefaultListItem.WithLevel(1).WithText("1"))
+	is = append(is, pterm.DefaultListItem.WithLevel(2).WithText("2"))
+	is = append(is, pterm.DefaultListItem.WithLevel(3).WithText("3"))
+	is = append(is, pterm.DefaultListItem.WithLevel(4).WithText("4"))
+	is = append(is, pterm.DefaultListItem.WithLevel(5).WithText("5"))
+
+	pterm.DefaultBulletList.WithItems(is).Render()
+
+	pterm.NewListFromString(`0
+ 1
+  2
+   3
+    4
+     5`, " ").Render()
+
+	pterm.NewListFromStrings([]string{"0", " 1", "  2", "   3", "    4", "     5"}, " ").Render()
+
+	pterm.DefaultBulletList.WithItems([]pterm.ListItem{
+		{Level: 0, Text: "Hello World"},
+		{Level: 1, Text: "Hello World"},
+		{Level: 1, Text: "Hello World"},
+		{Level: 2, Text: "Hello World"},
+		{Level: 3, Text: "Hello World"},
+		{Level: 4, Text: "Hello World"},
+		{Level: 5, Text: "Hello World"},
+		{Level: 6, Text: "Hello World"},
+	}).Render()
+
+	for {
+		for i := 0; i < pterm.GetTerminalWidth()-6; i++ {
+
+			pterm.ListItem{
+				Level: i,
+				Text:  "PENIS",
+			}.Render()
+
+			time.Sleep(time.Millisecond * 10)
+		}
+
+		for i := pterm.GetTerminalWidth() - 6; i > 0; i-- {
+
+			pterm.ListItem{
+				Level: i,
+				Text:  "PENIS",
+			}.Render()
+
+			time.Sleep(time.Millisecond * 10)
+		}
+	}
+}
