bff67d6df248dc529e4f85e6fc323edd08db6abf
MarvinJWendt <git@marvinjwendt.com>
2020-10-12 21:34:13 +0200

    feat: add `Srender` to `RenderPrinter` interface

--- interface_renderable_printer.go
+++ interface_renderable_printer.go
@@ -3,5 +3,9 @@ package pterm
 // RenderPrinter is used to display renderable content.
 // Example for renderable content is a Table.
 type RenderPrinter interface {
+	// Render the XXX to the terminal.
 	Render()
+
+	// Srender returns the rendered string of XXX.
+	Srender() string
 }
