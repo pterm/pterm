d61b107c590e255ef80d7e1e7dc26a5700cfca0f
MarvinJWendt <git@marvinjwendt.com>
2020-10-20 23:33:23 +0200

    test: fix internal percentage test

--- internal/percentage_test.go
+++ internal/percentage_test.go
@@ -44,7 +44,7 @@ func TestPercentageRound(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := PercentageRound(tt.args.total, tt.args.current, tt.args.max); got != tt.want {
+			if got := PercentageRound(tt.args.total, tt.args.current); got != tt.want {
 				t.Errorf("PercentageRound() = %v, want %v", got, tt.want)
 			}
 		})
