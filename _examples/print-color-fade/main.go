9b1630e043a3b23736a043373d86290e7f0d06dc
Florian Aust <flo.extra.email@gmail.com>
2020-10-21 14:47:57 +0200

    docs(examples): color fade demo, fade from cyan to magenta (#68)

--- _examples/print-color-fade/main.go
+++ _examples/print-color-fade/main.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	from := pterm.NewRGB(255, 0, 0)
-	to := pterm.NewRGB(0, 255, 0)
+	from := pterm.NewRGB(0, 255, 255)
+	to := pterm.NewRGB(255, 0, 255)
 	for i := 0; i < pterm.GetTerminalHeight(); i++ {
 		from.Fade(0, float32(pterm.GetTerminalHeight()), float32(i), to).Println("Hello, World!")
 	}
