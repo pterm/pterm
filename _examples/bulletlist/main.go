41a129fc7dfdb10366e4cbd28ad380cbe3f982ee
MarvinJWendt <git@marvinjwendt.com>
2020-10-26 17:01:50 +0100

    docs(examples): update `BulletListPrinter` example

--- _examples/bulletlist/main.go
+++ _examples/bulletlist/main.go
@@ -3,12 +3,15 @@ package main
 import "github.com/pterm/pterm"
 
 func main() {
+	// Print a list with different levels.
+	// Useful to generate lists automatically from data.
 	pterm.DefaultBulletList.WithItems([]pterm.BulletListItem{
 		{Level: 0, Text: "Level 0"},
 		{Level: 1, Text: "Level 1"},
 		{Level: 2, Text: "Level 2"},
 	}).Render()
 
+	// Convert a text to a list and print it.
 	pterm.NewListFromString(`0
  1
   2
