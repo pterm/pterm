1e9941a0f90fd16c42214df95479fd76a27b26d0
MarvinJWendt <git@marvinjwendt.com>
2020-10-26 17:27:38 +0100

    docs(examples): update `print-color-fade-multiple` example

--- _examples/print-color-fade-multiple/main.go
+++ _examples/print-color-fade-multiple/main.go
@@ -5,14 +5,18 @@ import (
 )
 
 func main() {
+	// Print info.
 	pterm.Info.Println("RGB colors only work in Terminals which support TrueColor.")
 
-	from := pterm.NewRGB(0, 255, 255)
-	to := pterm.NewRGB(255, 0, 255)
-	to2 := pterm.NewRGB(255, 0, 0)
-	to3 := pterm.NewRGB(0, 255, 0)
-	to4 := pterm.NewRGB(255, 255, 255)
+	from := pterm.NewRGB(0, 255, 255)  // This RGB value is used as the gradients start point.
+	to := pterm.NewRGB(255, 0, 255)    // This RGB value is used as the gradients first point.
+	to2 := pterm.NewRGB(255, 0, 0)     // This RGB value is used as the gradients second point.
+	to3 := pterm.NewRGB(0, 255, 0)     // This RGB value is used as the gradients third point.
+	to4 := pterm.NewRGB(255, 255, 255) // This RGB value is used as the gradients end point.
+
+	// For loop over the range of the terminal height.
 	for i := 0; i < pterm.GetTerminalHeight()-2; i++ {
+		// Print string which is colored with the faded RGB value.
 		from.Fade(0, float32(pterm.GetTerminalHeight()-2), float32(i), to, to2, to3, to4).Println("Hello, World!")
 	}
 }
