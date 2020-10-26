c9801184d0f4b42598cdd2a78489e8d2fd3c1754
MarvinJWendt <git@marvinjwendt.com>
2020-10-26 17:50:46 +0100

    docs(examples): update `Table` example

--- _examples/table/main.go
+++ _examples/table/main.go
@@ -3,6 +3,8 @@ package main
 import "github.com/pterm/pterm"
 
 func main() {
+	// Create a fork of the default table, fill it with data and print it.
+	// Data can also be generated and inserted later.
 	pterm.DefaultTable.WithHasHeader().WithData(pterm.TableData{
 		{"Firstname", "Lastname", "Email"},
 		{"Paul", "Dean", "nisi.dictum.augue@velitAliquam.co.uk"},
