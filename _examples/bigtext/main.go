bd6c0c067aaa9b946a066a0500b1ccc51f6a918d
MarvinJWendt <git@marvinjwendt.com>
2020-10-26 16:59:08 +0100

    docs(examples): update `BigTextPrinter` example

--- _examples/bigtext/main.go
+++ _examples/bigtext/main.go
@@ -3,8 +3,11 @@ package main
 import "github.com/pterm/pterm"
 
 func main() {
+	// Print a large text with the LetterStyle from the standard theme.
+	// Useful for title screens.
 	pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString("PTerm")).Render()
 
+	// Print a large text with differently colored letters.
 	pterm.DefaultBigText.WithLetters(
 		pterm.NewLettersFromStringWithStyle("P", pterm.NewStyle(pterm.FgCyan)),
 		pterm.NewLettersFromStringWithStyle("Term", pterm.NewStyle(pterm.FgLightMagenta))).
