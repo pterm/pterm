5cb6362e7dd9cdab54b2aa0200032793a96c4585
MarvinJWendt <git@marvinjwendt.com>
2020-10-29 12:59:34 +0100

    docs(examples): refactor `treelist` example

--- _examples/treelist/main.go
+++ _examples/treelist/main.go
@@ -7,16 +7,16 @@ import (
 func main() {
 
 	tis2 := pterm.LvlTreeListItems{
-		pterm.LvlTreeListItem{Level: 0, Text: "0"},
-		pterm.LvlTreeListItem{Level: 1, Text: "1"},
-		pterm.LvlTreeListItem{Level: 1, Text: "1.1"},
-		pterm.LvlTreeListItem{Level: 0, Text: "0.1"},
-		pterm.LvlTreeListItem{Level: 0, Text: "0.2"},
-		pterm.LvlTreeListItem{Level: 1, Text: "12"},
-		pterm.LvlTreeListItem{Level: 1, Text: "12.1"},
-		pterm.LvlTreeListItem{Level: 0, Text: "0.3"},
-		pterm.LvlTreeListItem{Level: 0, Text: "0.4"},
-		pterm.LvlTreeListItem{Level: 0, Text: "0.5"},
+		pterm.LvlTreeListItem{Level: 0, Text: "0.0"},
+		pterm.LvlTreeListItem{Level: 1, Text: "0.1"},
+		pterm.LvlTreeListItem{Level: 1, Text: "0.2"},
+		pterm.LvlTreeListItem{Level: 0, Text: "1.0"},
+		pterm.LvlTreeListItem{Level: 0, Text: "2.0"},
+		pterm.LvlTreeListItem{Level: 1, Text: "2.1"},
+		pterm.LvlTreeListItem{Level: 1, Text: "2.2"},
+		pterm.LvlTreeListItem{Level: 0, Text: "3.0"},
+		pterm.LvlTreeListItem{Level: 0, Text: "4.0"},
+		pterm.LvlTreeListItem{Level: 0, Text: "5.0"},
 	}
 
 	tis3 := tis2.ConvertLeveledListToTreeListItems(0, 0)
