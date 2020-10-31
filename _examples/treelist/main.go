9f7701e7b906059e80ab12fedcbd94f1534f7a2a
floaust <flo.extra.email@gmail.com>
2020-10-31 02:12:51 +0100

    docs(examples): add example for `Tree`

--- _examples/treelist/main.go
+++ _examples/treelist/main.go
@@ -6,35 +6,33 @@ import (
 
 func main() {
 
-	tis2 := pterm.LvlTreeListItems{
-		pterm.LvlTreeListItem{Level: 0, Text: "0.0"},
-		pterm.LvlTreeListItem{Level: 1, Text: "0.1"},
-		pterm.LvlTreeListItem{Level: 1, Text: "0.2"},
-		pterm.LvlTreeListItem{Level: 0, Text: "1.0"},
-		pterm.LvlTreeListItem{Level: 0, Text: "2.0"},
-		pterm.LvlTreeListItem{Level: 1, Text: "2.1"},
-		pterm.LvlTreeListItem{Level: 1, Text: "2.2"},
-		pterm.LvlTreeListItem{Level: 2, Text: "2.2.1"},
-		pterm.LvlTreeListItem{Level: 1, Text: "2.3"},
-		pterm.LvlTreeListItem{Level: 2, Text: "2.3.1"},
-		pterm.LvlTreeListItem{Level: 3, Text: "2.3.1.1"},
-		pterm.LvlTreeListItem{Level: 4, Text: "2.3.1.1.1"},
-		pterm.LvlTreeListItem{Level: 0, Text: "3.0"},
-		pterm.LvlTreeListItem{Level: 0, Text: "4.0"},
-		pterm.LvlTreeListItem{Level: 1, Text: "4.0"},
-		pterm.LvlTreeListItem{Level: 2, Text: "4.0.1"},
-		pterm.LvlTreeListItem{Level: 3, Text: "4.0.1.1"},
-		pterm.LvlTreeListItem{Level: 4, Text: "4.0.1.1.1"},
-		pterm.LvlTreeListItem{Level: 1, Text: "4.1"},
-		pterm.LvlTreeListItem{Level: 2, Text: "4.1.1"},
-		pterm.LvlTreeListItem{Level: 1, Text: "4.2"},
-		pterm.LvlTreeListItem{Level: 1, Text: "4.3"},
-		pterm.LvlTreeListItem{Level: 1, Text: "4.4"},
-		pterm.LvlTreeListItem{Level: 1, Text: "4.5"},
-		pterm.LvlTreeListItem{Level: 0, Text: "5.0"},
+	tis2 := pterm.LeveledList{
+		pterm.LeveledListItem{Level: 0, Text: "0.0"},
+		pterm.LeveledListItem{Level: 1, Text: "0.1"},
+		pterm.LeveledListItem{Level: 1, Text: "0.2"},
+		pterm.LeveledListItem{Level: 0, Text: "1.0"},
+		pterm.LeveledListItem{Level: 0, Text: "2.0"},
+		pterm.LeveledListItem{Level: 1, Text: "2.1"},
+		pterm.LeveledListItem{Level: 1, Text: "2.2"},
+		pterm.LeveledListItem{Level: 2, Text: "2.2.1"},
+		pterm.LeveledListItem{Level: 1, Text: "2.3"},
+		pterm.LeveledListItem{Level: 2, Text: "2.3.1"},
+		pterm.LeveledListItem{Level: 3, Text: "2.3.1.1"},
+		pterm.LeveledListItem{Level: 4, Text: "2.3.1.1.1"},
+		pterm.LeveledListItem{Level: 0, Text: "3.0"},
+		pterm.LeveledListItem{Level: 0, Text: "4.0"},
+		pterm.LeveledListItem{Level: 1, Text: "4.0"},
+		pterm.LeveledListItem{Level: 2, Text: "4.0.1"},
+		pterm.LeveledListItem{Level: 3, Text: "4.0.1.1"},
+		pterm.LeveledListItem{Level: 4, Text: "4.0.1.1.1"},
+		pterm.LeveledListItem{Level: 1, Text: "4.1"},
+		pterm.LeveledListItem{Level: 2, Text: "4.1.1"},
+		pterm.LeveledListItem{Level: 1, Text: "4.2"},
+		pterm.LeveledListItem{Level: 1, Text: "4.3"},
+		pterm.LeveledListItem{Level: 0, Text: "5.0"},
 	}
 
-	tis3 := pterm.ConvertLeveledListToTreeListItems(tis2)
+	tis3 := pterm.NewTreeFromLeveledList(tis2)
 
-	pterm.DefaultTreeList.WithItems(tis3).Render()
+	pterm.DefaultTree.WithRoot(tis3).Render()
 }
