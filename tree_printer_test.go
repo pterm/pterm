b47f93392d7c926fa28e90b186c5045e29e72e9f
floaust <flo.extra.email@gmail.com>
2020-10-31 03:11:15 +0100

    test(tree): add `Tree` tests

---
+++ tree_printer_test.go
@@ -0,0 +1,124 @@
+package pterm
+
+import (
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pterm/pterm/internal"
+)
+
+func TestTreePrinterNilPrint(t *testing.T) {
+	proxyToDevNull()
+	Tree{}.Render()
+	Tree{}.WithRoot(NewTreeFromLeveledList(LeveledList{LeveledListItem{Text: "Hello, World!", Level: 0}})).Render()
+}
+
+func TestTree_Render(t *testing.T) {
+	internal.TestPrintContains(t, func(w io.Writer, a interface{}) {
+		DefaultTree.WithRoot(NewTreeFromLeveledList([]LeveledListItem{
+			{Level: 0, Text: fmt.Sprint(a)},
+			{Level: 0, Text: "0.0"},
+			{Level: 1, Text: "0.1"},
+			{Level: 1, Text: "0.2"},
+			{Level: 0, Text: "1.0"},
+			{Level: 0, Text: "2.0"},
+			{Level: 1, Text: "2.1"},
+			{Level: 1, Text: "2.2"},
+			{Level: 2, Text: "2.2.1"},
+			{Level: 1, Text: "2.3"},
+		})).Render()
+	})
+}
+
+func TestTree_NewTreeFromLeveledList(t *testing.T) {
+	p := DefaultTree
+	p2 := p.WithRoot(NewTreeFromLeveledList(LeveledList{
+		LeveledListItem{Level: 0, Text: "0.0"},
+		LeveledListItem{Level: 1, Text: "0.1"},
+		LeveledListItem{Level: 1, Text: "0.2"},
+		LeveledListItem{Level: 0, Text: "1.0"},
+		LeveledListItem{Level: 0, Text: "2.0"},
+		LeveledListItem{Level: 1, Text: "2.1"},
+		LeveledListItem{Level: 1, Text: "2.2"},
+		LeveledListItem{Level: 2, Text: "2.2.1"},
+		LeveledListItem{Level: 1, Text: "2.3"}}))
+
+	assert.Equal(t, NewTreeFromLeveledList(LeveledList{
+		LeveledListItem{Level: 0, Text: "0.0"},
+		LeveledListItem{Level: 1, Text: "0.1"},
+		LeveledListItem{Level: 1, Text: "0.2"},
+		LeveledListItem{Level: 0, Text: "1.0"},
+		LeveledListItem{Level: 0, Text: "2.0"},
+		LeveledListItem{Level: 1, Text: "2.1"},
+		LeveledListItem{Level: 1, Text: "2.2"},
+		LeveledListItem{Level: 2, Text: "2.2.1"},
+		LeveledListItem{Level: 1, Text: "2.3"}}), p2.Root)
+	assert.Empty(t, p.Root)
+}
+
+func TestTree_WithHorizontalString(t *testing.T) {
+	p := Tree{}
+	p2 := p.WithHorizontalString("-")
+
+	assert.Equal(t, "-", p2.HorizontalString)
+	assert.Empty(t, p.HorizontalString)
+}
+
+func TestTree_WithRoot(t *testing.T) {
+	p := Tree{}
+	p2 := p.WithRoot(TreeNode{
+		Children: nil,
+		Text:     "Hello, World!",
+	})
+
+	assert.Equal(t, TreeNode{
+		Children: nil,
+		Text:     "Hello, World!",
+	}, p2.Root)
+	assert.Empty(t, p.Root)
+}
+
+func TestTree_WithStyle(t *testing.T) {
+	p := Tree{}
+	s := NewStyle(FgRed, BgRed, Bold)
+	p2 := p.WithStyle(s)
+
+	assert.Equal(t, s, p2.TreeStyle)
+	assert.Empty(t, p.TreeStyle)
+}
+
+func TestTree_WithTextStyle(t *testing.T) {
+	p := Tree{}
+	s := NewStyle(FgRed, BgRed, Bold)
+	p2 := p.WithTextStyle(s)
+
+	assert.Equal(t, s, p2.TextStyle)
+	assert.Empty(t, p.TextStyle)
+}
+
+func TestTree_WithTopRightCornerString(t *testing.T) {
+	p := Tree{}
+	p2 := p.WithTopRightCornerString("-")
+
+	assert.Equal(t, "-", p2.TopRightCornerString)
+	assert.Empty(t, p.TopRightCornerString)
+}
+
+func TestTree_WithTopRightDownStringOngoing(t *testing.T) {
+	p := Tree{}
+	p2 := p.WithTopRightDownStringOngoing("-")
+
+	assert.Equal(t, "-", p2.TopRightDownString)
+	assert.Empty(t, p.TopRightDownString)
+}
+
+func TestTree_WithVerticalString(t *testing.T) {
+	p := Tree{}
+	p2 := p.WithVerticalString("-")
+
+	assert.Equal(t, "-", p2.VerticalString)
+	assert.Empty(t, p.VerticalString)
+}
