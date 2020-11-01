ce9916ce9d327f009b07ab84964455cc40060de2
floaust <flo.extra.email@gmail.com>
2020-11-01 14:38:36 +0100

    refactor(theme): change ProgressbarBarStyle FgLightCyan to FgCyan color in `Theme`

--- theme.go
+++ theme.go
@@ -20,7 +20,7 @@ var (
 		DescriptionMessageStyle: Style{FgDefault},
 		DescriptionPrefixStyle:  Style{FgLightWhite, BgDarkGray},
 		ScopeStyle:              Style{FgGray},
-		ProgressbarBarStyle:     Style{FgLightCyan},
+		ProgressbarBarStyle:     Style{FgCyan},
 		ProgressbarTitleStyle:   Style{FgLightCyan},
 		HeaderTextStyle:         Style{FgLightWhite, Bold},
 		HeaderBackgroundStyle:   Style{BgGray},
