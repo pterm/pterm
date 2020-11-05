1e5f0d9095cc377916365dc18b7c86f44debad99
MarvinJWendt <git@marvinjwendt.com>
2020-11-05 02:39:14 +0100

    fix: fix internal `GetStringMaxWidth` max width

--- internal/max_text_width.go
+++ internal/max_text_width.go
@@ -12,7 +12,7 @@ func GetStringMaxWidth(s string) int {
 	var max int
 	ss := strings.Split(s, "\n")
 	for _, s2 := range ss {
-		if runewidth.StringWidth(s2) > max {
+		if runewidth.StringWidth(color.ClearCode(s2)) > max {
 			max = runewidth.StringWidth(color.ClearCode(s2))
 		}
 	}
