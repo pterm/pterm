afd9d7f8fe71900746b098f6be9aa06206a6aba9
MarvinJWendt <git@marvinjwendt.com>
2020-11-12 17:47:37 +0100

    ci: count tests from all packages

--- ci/main.go
+++ ci/main.go
@@ -55,7 +55,7 @@ func main() {
 	unittestTimeout := make(chan string, 1)
 
 	go func() {
-		cmd := exec.Command("bash", "-c", "go test -v -p 1 .")
+		cmd := exec.Command("bash", "-c", "go test -v -p 1 ./...")
 		json, _ := cmd.CombinedOutput()
 		unitTestCount := fmt.Sprint(strings.Count(string(json), "RUN"))
 		log.Output(4, "### Unit test count: "+unitTestCount)
