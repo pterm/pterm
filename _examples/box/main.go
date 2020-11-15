6638dd740acab617708ddced05d890dba5e14ad6
floaust <flo.extra.email@gmail.com>
2020-11-16 00:29:59 +0100

    docs(examples): change example for `BoxPrinter`

--- _examples/box/main.go
+++ _examples/box/main.go
@@ -3,14 +3,14 @@ package main
 import "github.com/pterm/pterm"
 
 func main() {
-	panel1, _ := pterm.DefaultBox.WithText("Lorem ipsum dolor sit amet, \nconsectetur adipiscing elit, \nsed do eiusmod tempor incididunt \nut labore et dolore \nmagna aliqua.").Srender()
-	panel2, _ := pterm.DefaultBox.WithText("Ut enim ad minim veniam, \nquis nostrud exercitation \nullamco laboris \nnisi ut aliquip \nex ea commodo \nconsequat.").Srender()
-	panel3, _ := pterm.DefaultBox.WithText("Duis aute irure \ndolor in reprehenderit \nin voluptate velit esse cillum \ndolore eu fugiat \nnulla pariatur.").Srender()
+	panel1 := pterm.DefaultBox.Sprint("Lorem ipsum dolor sit amet, \nconsectetur adipiscing elit, \nsed do eiusmod tempor incididunt \nut labore et dolore \nmagna aliqua.")
+	panel2 := pterm.DefaultBox.Sprint("Ut enim ad minim veniam, \nquis nostrud exercitation \nullamco laboris \nnisi ut aliquip \nex ea commodo \nconsequat.")
+	panel3 := pterm.DefaultBox.Sprint("Duis aute irure \ndolor in reprehenderit \nin voluptate velit esse cillum \ndolore eu fugiat \nnulla pariatur.")
 
 	panels, _ := pterm.DefaultPanel.WithPanels(pterm.Panels{
 		{{panel1}, {panel2}},
 		{{panel3}},
 	}).Srender()
 
-	_ = pterm.DefaultBox.WithText(panels).WithBottomPadding(0).Render()
+	pterm.DefaultBox.WithBottomPadding(0).Println(panels)
 }
