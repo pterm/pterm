600c37867d9218e80f99cc65b2c912bab2db5692
MarvinJWendt <git@marvinjwendt.com>
2021-04-06 17:00:18 +0200

    test: add interface tests for `Color` and `RGB`

--- pterm_test.go
+++ pterm_test.go
@@ -41,7 +41,7 @@ func TestEnableStyling(t *testing.T) {
 
 func TestInterfaceImplementation(t *testing.T) {
 	// If a printer doesn't fit into the slice, the printer doesn't has the right interface anymore.
-	_ = []TextPrinter{&DefaultBasicText, DefaultBox, DefaultCenter, &DefaultHeader, &DefaultParagraph, &Info, &DefaultSection}
+	_ = []TextPrinter{&DefaultBasicText, DefaultBox, DefaultCenter, &DefaultHeader, &DefaultParagraph, &Info, &DefaultSection, FgRed, NewRGB(0, 0, 0)}
 	_ = []LivePrinter{DefaultProgressbar, &DefaultSpinner}
 	_ = []RenderPrinter{DefaultBarChart, DefaultBigText, DefaultBulletList, DefaultPanel, DefaultTable, DefaultTree}
 }
