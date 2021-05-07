a905536669b7ccf78a46db9c77d63bd14e2bcc3d
floaust <flo.extra.email@gmail.com>
2021-05-07 22:13:31 +0200

    feat(prefix): add `Sprintfln` and `Printfln` function

--- prefix_printer.go
+++ prefix_printer.go
@@ -207,6 +207,12 @@ func (p PrefixPrinter) Sprintf(format string, a ...interface{}) string {
 	return p.Sprint(Sprintf(format, a...))
 }
 
+// Sprintfln formats according to a format specifier and returns the resulting string.
+// Spaces are always added between operands and a newline is appended.
+func (p PrefixPrinter) Sprintfln(format string, a ...interface{}) string {
+	return p.Sprint(Sprintf(format, a...) + "\n")
+}
+
 // Print formats using the default formats for its operands and writes to standard output.
 // Spaces are added between operands when neither is a string.
 // It returns the number of bytes written and any write error encountered.
@@ -242,6 +248,15 @@ func (p *PrefixPrinter) Printf(format string, a ...interface{}) *TextPrinter {
 	}
 	Print(p.Sprintf(format, a...))
 	checkFatal(p)
+	return &tp
+}
+
+// Printfln formats according to a format specifier and writes to standard output.
+// Spaces are always added between operands and a newline is appended.
+// It returns the number of bytes written and any write error encountered.
+func (p *PrefixPrinter) Printfln(format string, a ...interface{}) *TextPrinter {
+	Print(p.Sprintfln(format, a...))
+	tp := TextPrinter(p)
 	return &tp
 }
 
