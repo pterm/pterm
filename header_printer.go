8f2e136a5181934db9f2657dc4b5be14ab7352fb
MarvinJWendt <git@marvinjwendt.com>
2021-05-13 17:44:33 +0200

    feat(headerprinter): add raw output mode

--- header_printer.go
+++ header_printer.go
@@ -53,6 +53,10 @@ func (p HeaderPrinter) WithFullWidth(b ...bool) *HeaderPrinter {
 // Sprint formats using the default formats for its operands and returns the resulting string.
 // Spaces are added between operands when neither is a string.
 func (p HeaderPrinter) Sprint(a ...interface{}) string {
+	if RawOutput {
+		return Sprint(a...)
+	}
+
 	if p.TextStyle == nil {
 		p.TextStyle = NewStyle()
 	}
