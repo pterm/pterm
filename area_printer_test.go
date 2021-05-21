75c3f288d89e920525207fc6e3815a1e3bf27f69
MarvinJWendt <git@marvinjwendt.com>
2021-05-21 18:54:34 +0200

    test(area): add `AreaPrinter` tests

--- area_printer_test.go
+++ area_printer_test.go
@@ -1,1 +1,43 @@
 package pterm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAreaPrinter_NilPrint(t *testing.T) {
+	p := AreaPrinter{}
+	p.Update("asd")
+}
+
+func TestAreaPrinter_GenericStart(t *testing.T) {
+	p := DefaultArea
+	p.GenericStart()
+}
+
+func TestAreaPrinter_GenericStartRawOutput(t *testing.T) {
+	DisableStyling()
+	p := DefaultArea
+	p.GenericStart()
+	EnableStyling()
+}
+
+func TestAreaPrinter_GenericStop(t *testing.T) {
+	p := DefaultArea
+	p.GenericStop()
+}
+
+func TestAreaPrinter_WithRemoveWhenDone(t *testing.T) {
+	p := AreaPrinter{}
+	p2 := p.WithRemoveWhenDone()
+
+	assert.True(t, p2.RemoveWhenDone)
+}
+
+func TestAreaPrinter_RemoveWhenDone(t *testing.T) {
+	a, _ := DefaultArea.WithRemoveWhenDone().Start()
+
+	a.Update("asd")
+	a.Stop()
+}
