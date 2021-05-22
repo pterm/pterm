5df0054b805e1259ad52d8a73a101143d9a54789
Marvin Wendt <git@marvinjwendt.com>
2021-05-22 21:04:53 +0200

    feat(putils): add `putils` package (#206)

---
+++ putils/run-with-spinner.go
@@ -0,0 +1,21 @@
+package putils
+
+import "github.com/pterm/pterm"
+
+// RunWithSpinner starts a spinner, then runs a function and after the function is done, the spinner will stop again.
+func RunWithSpinner(spinner *pterm.SpinnerPrinter, f func(spinner *pterm.SpinnerPrinter) error) error {
+	s, err := spinner.Start()
+	if err != nil {
+		return err
+	}
+
+	err = f(s)
+	s.Stop()
+
+	return err
+}
+
+// RunWithDefaultSpinner starts a default spinner, then runs a function and after the function is done, the spinner will stop again.
+func RunWithDefaultSpinner(initzialSpinnerText string, f func(spinner *pterm.SpinnerPrinter) error) error {
+	return RunWithSpinner(pterm.DefaultSpinner.WithText(initzialSpinnerText), f)
+}
