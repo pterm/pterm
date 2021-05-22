ca2b452665869fbefe862dd8a1f6d35b95fbae8d
MarvinJWendt <git@marvinjwendt.com>
2021-05-22 18:09:05 +0200

    docs(examples): update area printer example to use `Center` option

--- _examples/area/main.go
+++ _examples/area/main.go
@@ -10,10 +10,9 @@ func main() {
 	pterm.Info.Println("The previous text will stay in place, while the area updates.")
 	pterm.Print("\n\n") // Add two new lines as spacer.
 
-	area, _ := pterm.DefaultArea.Start() // Start the Area printer.
+	area, _ := pterm.DefaultArea.WithCenter().Start() // Start the Area printer, with the Center option.
 	for i := 0; i < 10; i++ {
 		str, _ := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString(time.Now().Format("15:04:05"))).Srender() // Save current time in str.
-		str = pterm.DefaultCenter.Sprint(str)                                                                           // Center str.
 		area.Update(str)                                                                                                // Update Area contents.
 		time.Sleep(time.Second)
 	}
