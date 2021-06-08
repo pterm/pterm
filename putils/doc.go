f005d62f075d7e0047c7c6179348c2a67783826f
MarvinJWendt <git@marvinjwendt.com>
2021-06-08 23:55:00 +0200

    docs(putils): update putils docs

--- putils/doc.go
+++ putils/doc.go
@@ -1,3 +1,4 @@
 // Package putils contains utility functions for PTerm, to make it's usage even easier!
-// It helps you to simplify various scenarios for which PTerm is frequently used.
+// It contains pre-made functions, that utilize PTerm, to print various stuff to the terminal.
+// You can use PUtils, to simplify various scenarios for which PTerm is frequently used.
 package putils
