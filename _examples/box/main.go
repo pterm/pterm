d00ae12a513cdc36fcf889a0513372c7301604b9
floaust <flo.extra.email@gmail.com>
2021-06-11 16:38:47 +0200

    docs(boxprinter): add title & title center position to `BoxPrinter`

--- _examples/box/main.go
+++ _examples/box/main.go
@@ -7,7 +7,7 @@ func main() {
 
 	panel1 := pterm.DefaultBox.Sprint("Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit,\nsed do eiusmod tempor incididunt\nut labore et dolore\nmagna aliqua.")
 	panel2 := pterm.DefaultBox.WithTitle("title").Sprint("Ut enim ad minim veniam,\nquis nostrud exercitation\nullamco laboris\nnisi ut aliquip\nex ea commodo\nconsequat.")
-	panel3 := pterm.DefaultBox.Sprint("Duis aute irure\ndolor in reprehenderit\nin voluptate velit esse cillum\ndolore eu fugiat\nnulla pariatur.")
+	panel3 := pterm.DefaultBox.WithTitle("bottom center title").WithTitleBottomCenter().Sprint("Duis aute irure\ndolor in reprehenderit\nin voluptate velit esse cillum\ndolore eu fugiat\nnulla pariatur.")
 
 	panels, _ := pterm.DefaultPanel.WithPanels(pterm.Panels{
 		{{Data: panel1}, {Data: panel2}},
