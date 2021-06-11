49a9a23834246079d93ebad8f415949ad34a634d
floaust <flo.extra.email@gmail.com>
2021-06-11 16:38:47 +0200

    docs(boxprinter): add title & title center position to `BoxPrinter`

--- internal/title_in_line.go
+++ internal/title_in_line.go
@@ -10,10 +10,21 @@ import (
 func AddTitleToLine(title, line string, length int, left bool) string {
 	var ret string
 	if left {
-		ret += line + " " + title + " " + strings.Repeat(line, length-(3+len(color.ClearCode(title))))
+		ret += line + " " + title + " " + line + strings.Repeat(line, length-(4+len(color.ClearCode(title))))
 	} else {
-		ret += strings.Repeat(line, length-(3+len(color.ClearCode(title)))) + " " + title + " " + line
+		ret += strings.Repeat(line, length-(4+len(color.ClearCode(title)))) + line + " " + title + " " + line
 	}
+
+	return ret
+}
+
+// AddTitleToLineCenter adds a title to the center of a line ex: "─ This is the title ──────"
+func AddTitleToLineCenter(title, line string, length int) string {
+	var ret string
+	repeatString := length - (4 + len(color.ClearCode(title)))
+	unevenRepeatString := repeatString % 2
+
+	ret += strings.Repeat(line, repeatString/2) + line + " " + title + " " + line + strings.Repeat(line, repeatString/2+unevenRepeatString)
 
 	return ret
 }
