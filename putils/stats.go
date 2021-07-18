27e7cf68d2b40b4c0e02f7f9bc1e33613f50b34a
MarvinJWendt <git@marvinjwendt.com>
2021-07-18 02:57:18 +0200

    feat(putils): add `PrintAverageExecutionTime`

---
+++ putils/stats.go
@@ -0,0 +1,30 @@
+package putils
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pterm/pterm"
+)
+
+// PrintAverageExecutionTime times the average execution time of a function.
+func PrintAverageExecutionTime(count int, f func(i int) error) error {
+	var total time.Duration
+	for i := 0; i < count; i++ {
+		start := time.Now()
+		err := f(i)
+		duration := time.Since(start)
+
+		if err != nil {
+			return fmt.Errorf("error while calculating average execution time: %w", err)
+		}
+
+		total += duration
+	}
+
+	averageExecutionTime := total / time.Duration(count)
+
+	pterm.Printfln(pterm.Cyan("Average execution time: %s"), pterm.LightCyan(averageExecutionTime))
+
+	return nil
+}
