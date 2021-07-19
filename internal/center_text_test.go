1a64a50bddffd89bf80a302e622ce45ebb3f928c
MarvinJWendt <git@marvinjwendt.com>
2021-07-19 15:50:43 +0200

    test: fix internal test import cycle

--- internal/center_text_test.go
+++ internal/center_text_test.go
@@ -1,12 +1,13 @@
-package internal
+package internal_test
 
 import (
 	"testing"
 
 	"github.com/MarvinJWendt/testza"
+	"github.com/pterm/pterm/internal"
 )
 
 func TestCenterText(t *testing.T) {
-	testza.AssertEqual(t, "  Hello Wolrd  \n      !!!      ", CenterText("Hello Wolrd\n!!!", 15))
-	testza.AssertEqual(t, "Hello\n Wolr\n  d  \n !!! ", CenterText("Hello Wolrd\n!!!", 5))
+	testza.AssertEqual(t, "  Hello Wolrd  \n      !!!      ", internal.CenterText("Hello Wolrd\n!!!", 15))
+	testza.AssertEqual(t, "Hello\n Wolr\n  d  \n !!! ", internal.CenterText("Hello Wolrd\n!!!", 5))
 }
--- internal/map_range_to_range_test.go
+++ internal/map_range_to_range_test.go
@@ -1,14 +1,15 @@
-package internal
+package internal_test
 
 import (
 	"testing"
 
 	"github.com/MarvinJWendt/testza"
+	"github.com/pterm/pterm/internal"
 )
 
 func TestMapRangeToRange(t *testing.T) {
-	testza.AssertEqual(t, 127, MapRangeToRange(0, 100, 0, 255, 50))
-	testza.AssertEqual(t, 127, MapRangeToRange(0, 400, 0, 255, 200))
-	testza.AssertEqual(t, 127, MapRangeToRange(-200, 200, 0, 255, 0))
-	testza.AssertEqual(t, 127, MapRangeToRange(0, 200.123, 0, 254.3, 100))
+	testza.AssertEqual(t, 127, internal.MapRangeToRange(0, 100, 0, 255, 50))
+	testza.AssertEqual(t, 127, internal.MapRangeToRange(0, 400, 0, 255, 200))
+	testza.AssertEqual(t, 127, internal.MapRangeToRange(-200, 200, 0, 255, 0))
+	testza.AssertEqual(t, 127, internal.MapRangeToRange(0, 200.123, 0, 254.3, 100))
 }
