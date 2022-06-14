c29360dbf709a88fccb227769c7105acba48a949
Marvin Wendt <marvin.wendt@mail.schwarz>
2022-06-14 15:27:35 +0200

    moved `NewLettersFromString` and `NewLettersFromStringWithStyle` to `putils`

--- bigtext_printer.go
+++ bigtext_printer.go
@@ -14,27 +14,6 @@ import (
 // Letters is a slice of Letter.
 type Letters []Letter
 
-// NewLettersFromString creates a Letters object from a string, which is prefilled with the LetterStyle from ThemeDefault.
-// You can override the ThemeDefault LetterStyle if you want to.
-func NewLettersFromString(text string) Letters {
-	return NewLettersFromStringWithStyle(text, &ThemeDefault.LetterStyle)
-}
-
-// NewLettersFromStringWithStyle creates a Letters object from a string and applies a Style to it.
-func NewLettersFromStringWithStyle(text string, style *Style) Letters {
-	s := strings.Split(text, "")
-	l := Letters{}
-
-	for _, s2 := range s {
-		l = append(l, Letter{
-			String: s2,
-			Style:  style,
-		})
-	}
-
-	return l
-}
-
 // NewLettersFromStringWithRGB creates a Letters object from a string and applies an RGB color to it (overwrites style).
 func NewLettersFromStringWithRGB(text string, rgb RGB) Letters {
 	s := strings.Split(text, "")
---
+++ deprecated.go
@@ -0,0 +1,28 @@
+package pterm
+
+import "strings"
+
+// NewLettersFromString creates a Letters object from a string, which is prefilled with the LetterStyle from ThemeDefault.
+// You can override the ThemeDefault LetterStyle if you want to.
+//
+// Deprecated: use putils.NewLettersFromString instead.
+func NewLettersFromString(text string) Letters {
+	return NewLettersFromStringWithStyle(text, &ThemeDefault.LetterStyle)
+}
+
+// NewLettersFromStringWithStyle creates a Letters object from a string and applies a Style to it.
+//
+// Deprecated: use putils.NewLettersFromStringWithStyle instead.
+func NewLettersFromStringWithStyle(text string, style *Style) Letters {
+	s := strings.Split(text, "")
+	l := Letters{}
+
+	for _, s2 := range s {
+		l = append(l, Letter{
+			String: s2,
+			Style:  style,
+		})
+	}
+
+	return l
+}
