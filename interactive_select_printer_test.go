c91ee03fcb4273a6ce8b8a3dc0d0db6763a10178
Marco Ponton <marco.ponton@gmail.com>
2022-11-18 16:18:17 -0500

    fix: slice bounds out of range on select printer
    Closes #419

--- interactive_select_printer_test.go
+++ interactive_select_printer_test.go
@@ -12,12 +12,12 @@ import (
 
 func TestInteractiveSelectPrinter_Show(t *testing.T) {
 	go func() {
-		keyboard.SimulateKeyPress(keys.Down)
-		keyboard.SimulateKeyPress(keys.Down)
+		keyboard.SimulateKeyPress(keys.Up)
+		keyboard.SimulateKeyPress(keys.Up)
 		keyboard.SimulateKeyPress(keys.Enter)
 	}()
-	result, _ := pterm.DefaultInteractiveSelect.WithOptions([]string{"a", "b", "c", "d", "e"}).WithDefaultOption("b").Show()
-	testza.AssertEqual(t, "d", result)
+	result, _ := pterm.DefaultInteractiveSelect.WithOptions([]string{"a", "b", "c", "d", "e", "f"}).WithDefaultOption("e").Show()
+	testza.AssertEqual(t, "c", result)
 }
 
 func TestInteractiveSelectPrinter_WithDefaultText(t *testing.T) {
