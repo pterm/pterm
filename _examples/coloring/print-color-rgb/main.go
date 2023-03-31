d2458083d093dcf86db4890c2e614773071a838e
MarvinJWendt <git@marvinjwendt.com>
2023-04-01 01:08:51 +0200

    feat(rgb): made it possible to use RGB colors as background

--- _examples/coloring/print-color-rgb/main.go
+++ _examples/coloring/print-color-rgb/main.go
@@ -7,5 +7,5 @@ func main() {
 	// NOTICE: This only works with terminals which support TrueColor.
 	pterm.NewRGB(178, 44, 199).Println("This text is printed with a custom RGB!")
 	pterm.NewRGB(15, 199, 209).Println("This text is printed with a custom RGB!")
-	pterm.NewRGB(201, 144, 30).Println("This text is printed with a custom RGB!")
+	pterm.NewRGB(201, 144, 30, true).Println("This text is printed with a custom RGB background!")
 }
