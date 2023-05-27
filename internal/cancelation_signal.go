5cd27f2218d48b6ddb3fbc59908be074a0068dca
Roland Lammel <rl@neotel.at>
2023-05-27 20:16:04 +0200

    Default to no action on cancelation to avoid unexpected exit

--- internal/cancelation_signal.go
+++ internal/cancelation_signal.go
@@ -1,22 +1,16 @@
 package internal
 
-import "os"
-
 // NewCancelationSignal for keeping track of a cancelation
-func NewCancelationSignal(exitFunc func()) (func(), func()) {
+func NewCancelationSignal(interruptFunc func()) (func(), func()) {
 	canceled := false
 
 	cancel := func() {
 		canceled = true
 	}
 
-	if exitFunc == nil {
-		exitFunc = func() { os.Exit(1) }
-	}
-
 	exit := func() {
-		if canceled {
-			exitFunc()
+		if canceled && interruptFunc != nil {
+			interruptFunc()
 		}
 	}
 
