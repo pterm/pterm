55b99d7bd609fd8d070a9f17925d0608f398e80e
Florian Aust <flo.extra.email@gmail.com>
2023-10-08 19:25:05 +0200

    feat(heatmap): change and add demos

--- _examples/heatmap/demo/main.go
+++ _examples/heatmap/demo/main.go
@@ -18,60 +18,6 @@ func main() {
 		YAxis: []string{"1", "2", "3", "4", "5"},
 	}
 
-	table := pterm.DefaultHeatmap.WithData(data).WithBoxed(false).WithAxisData(headerData).WithRGB().WithGrid(false)
+	table := pterm.DefaultHeatmap.WithAxisData(headerData).WithData(data)
 	table.Render()
-
-	table = pterm.DefaultHeatmap.WithData(data).WithBoxed(false).WithAxisData(headerData).WithGrid(false)
-	table.Render()
-
-	table = pterm.DefaultHeatmap.WithData(data).WithBoxed().WithAxisData(headerData).WithRGB()
-	table.Render()
-
-	table = pterm.DefaultHeatmap.WithData(data).WithBoxed().WithAxisData(headerData).WithRGB().WithLegendOnlyColoredCells()
-	table.Render()
-
-	table = pterm.DefaultHeatmap.WithData(data).WithBoxed().WithAxisData(headerData)
-	table.Render()
-
-	table3 := pterm.DefaultHeatmap.WithData(data).WithBoxed(false).WithAxisData(headerData).WithRGB()
-	table3.Render()
-
-	table2 := pterm.DefaultHeatmap.WithData(data).WithBoxed(false).WithRGB()
-	table2.Render()
-
-	table2 = pterm.DefaultHeatmap.WithData(data).WithBoxed().WithRGB()
-	table2.Render()
-
-	table = pterm.DefaultHeatmap.WithData(data).WithBoxed(false).WithRGB().WithGrid(false)
-	table.Render()
-
-	table2 = pterm.DefaultHeatmap.WithData(data).WithBoxed(false).WithRGB().WithAxisData(headerData).WithOnlyColoredCells().WithCellSize(3)
-	table2.Render()
-
-	table2 = pterm.DefaultHeatmap.WithData(data).WithBoxed(false).WithRGB().WithOnlyColoredCells().WithCellSize(5)
-	table2.Render()
-
-	table2 = pterm.DefaultHeatmap.WithData(data).WithRGB().WithCellSize(3).WithGrid(false).WithAxisData(headerData).WithOnlyColoredCells()
-	table2.Render()
-
-	table2 = pterm.DefaultHeatmap.WithData(data).WithRGB().WithCellSize(3).WithGrid(false).WithAxisData(headerData).WithOnlyColoredCells().WithLegendOnlyColoredCells()
-	table2.Render()
-
-	table2 = pterm.DefaultHeatmap.WithData(data).WithCellSize(3).WithGrid(false).WithAxisData(headerData).WithOnlyColoredCells()
-	table2.Render()
-
-	table2 = pterm.DefaultHeatmap.WithData(data).WithCellSize(3).WithGrid(false).WithAxisData(headerData).WithOnlyColoredCells().WithLegendOnlyColoredCells()
-	table2.Render()
-
-	table2 = pterm.DefaultHeatmap.WithData(data).WithCellSize(3).WithGrid(false).WithAxisData(headerData).WithOnlyColoredCells().WithLegend(false)
-	table2.Render()
-
-	table15 := pterm.DefaultHeatmap.WithData(data).WithCellSize(3).WithGrid(false).WithAxisData(headerData).WithOnlyColoredCells().WithLegendOnlyColoredCells().WithBoxed()
-	table15.Render()
-
-	table4 := pterm.DefaultHeatmap.WithData(data).WithBoxed().WithAxisData(headerData).WithRGB().WithLegendOnlyColoredCells().WithLegendTag("custom tag")
-	table4.Render()
-
-	table5 := pterm.DefaultHeatmap.WithData(data)
-	table5.Render()
 }
