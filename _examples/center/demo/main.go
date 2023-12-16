50ade079e737959e6485622f9345103db99906e7
MarvinJWendt <git@marvinjwendt.com>
2023-12-16 20:36:33 +0100

    example: fixed example

--- _examples/center/demo/main.go
+++ _examples/center/demo/main.go
@@ -10,7 +10,7 @@ func main() {
 	pterm.DefaultCenter.Println("This text is centered!\nIt centers the whole block by default.\nIn that way you can do stuff like this:")
 
 	// Generate BigLetters and store in 's'
-	s, _ := pterm.DefaultBigText.WithLetters(putils.LettersFromStringFromString("PTerm")).Srender()
+	s, _ := pterm.DefaultBigText.WithLetters(putils.LettersFromString("PTerm")).Srender()
 
 	// Print the BigLetters 's' centered in the terminal
 	pterm.DefaultCenter.Println(s)
