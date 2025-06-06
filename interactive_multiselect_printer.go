fb12e6923774f22563e5ab9bf3b91f0ab8fd6e7b
Marvin Wendt <git@marvinjwendt.com>
2025-06-06 21:22:24 +0200

    feat(multiselect): optionally show selected options (#719)
    * Transfer of @prnvbn work along with @MarvinJWendt changes.
    
    * golangci-lint adjustment
    
    Error fixed: emptyStringTest: replace `len(p.fuzzySearchString) > 0` with `p.fuzzySearchString != ""` (gocritic)
    
    ---------
    
    Co-authored-by: Tim Afanasiev <84281349+devnyxie@users.noreply.github.com>

--- interactive_multiselect_printer.go
+++ interactive_multiselect_printer.go
@@ -33,17 +33,18 @@ var (
 
 // InteractiveMultiselectPrinter is a printer for interactive multiselect menus.
 type InteractiveMultiselectPrinter struct {
-	DefaultText     string
-	TextStyle       *Style
-	Options         []string
-	OptionStyle     *Style
-	DefaultOptions  []string
-	MaxHeight       int
-	Selector        string
-	SelectorStyle   *Style
-	Filter          bool
-	Checkmark       *Checkmark
-	OnInterruptFunc func()
+	DefaultText         string
+	TextStyle           *Style
+	Options             []string
+	OptionStyle         *Style
+	DefaultOptions      []string
+	MaxHeight           int
+	Selector            string
+	SelectorStyle       *Style
+	Filter              bool
+	Checkmark           *Checkmark
+	OnInterruptFunc     func()
+	ShowSelectedOptions bool
 
 	selectedOption        int
 	selectedOptions       []int
@@ -111,9 +112,15 @@ func (p InteractiveMultiselectPrinter) WithCheckmark(checkmark *Checkmark) *Inte
 	return &p
 }
 
-// OnInterrupt sets the function to execute on exit of the input reader
+// WithOnInterruptFunc sets the function to execute on exit of the input reader
 func (p InteractiveMultiselectPrinter) WithOnInterruptFunc(exitFunc func()) *InteractiveMultiselectPrinter {
 	p.OnInterruptFunc = exitFunc
+	return &p
+}
+
+// WithShowSelectedOptions shows the selected options at the bottom if the menu
+func (p InteractiveMultiselectPrinter) WithShowSelectedOptions(b ...bool) *InteractiveMultiselectPrinter {
+	p.ShowSelectedOptions = internal.WithBoolean(b)
 	return &p
 }
 
@@ -380,9 +387,19 @@ func (p *InteractiveMultiselectPrinter) renderSelectMenu() string {
 
 	help := fmt.Sprintf("%s: %s | %s: %s | left: %s | right: %s", p.KeySelect, Bold.Sprint("select"), p.KeyConfirm, Bold.Sprint("confirm"), Bold.Sprint("none"), Bold.Sprint("all"))
 	if p.Filter {
-		help += fmt.Sprintf("| type to %s", Bold.Sprint("filter"))
+		help += fmt.Sprintf(" | type to %s", Bold.Sprint("filter"))
 	}
-	content.WriteString(ThemeDefault.SecondaryStyle.Sprintfln("%s", help))
+	content.WriteString(ThemeDefault.SecondaryStyle.Sprintfln(help))
+
+	// Optionally, add selected options to the menu
+	if p.ShowSelectedOptions && len(p.selectedOptions) > 0 {
+		selected := make([]string, len(p.selectedOptions))
+		for i, optIdx := range p.selectedOptions {
+			selected[i] = p.Options[optIdx]
+		}
+
+		content.WriteString(ThemeDefault.SecondaryStyle.Sprint("Selected: ") + Green(strings.Join(selected, Gray(", "))) + "\n")
+	}
 
 	return content.String()
 }
