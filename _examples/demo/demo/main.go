96543163c2b9788509bad674985ba0fe03633079
Marvin Wendt <git@marvinjwendt.com>
2025-10-01 23:03:08 +0200

    chore(demo): replace 'time.Second' with 'second' for consistency (#742)

--- _examples/demo/demo/main.go
+++ _examples/demo/demo/main.go
@@ -39,7 +39,7 @@ func main() {
 
 		logger.Trace("Doing not so important stuff", logger.Args("priority", "super low"))
 
-		time.Sleep(time.Second * 3)
+		time.Sleep(second * 3)
 
 		interstingStuff := map[string]any{
 			"when were crayons invented":  "1903",
@@ -47,14 +47,14 @@ func main() {
 			"is this interesting":         true,
 		}
 		logger.Debug("This might be interesting", logger.ArgsFromMap(interstingStuff))
-		time.Sleep(time.Second * 3)
+		time.Sleep(second * 3)
 
 		logger.Info("That was actually interesting", logger.Args("such", "wow"))
-		time.Sleep(time.Second * 3)
+		time.Sleep(second * 3)
 		logger.Warn("Oh no, I see an error coming to us!", logger.Args("speed", 88, "measures", "mph"))
-		time.Sleep(time.Second * 3)
+		time.Sleep(second * 3)
 		logger.Error("Damn, here it is!", logger.Args("error", "something went wrong"))
-		time.Sleep(time.Second * 3)
+		time.Sleep(second * 3)
 		logger.Info("But what's really cool is, that you can print very long logs, and PTerm will automatically wrap them for you! Say goodbye to text, that has weird line breaks!", logger.Args("very", "long"))
 	})
 
@@ -95,7 +95,7 @@ func main() {
 		for i := 0; i < 10; i++ {
 			str, _ := pterm.DefaultBigText.WithLetters(putils.LettersFromString(time.Now().Format("15:04:05"))).Srender() // Save current time in str.
 			area.Update(str)                                                                                              // Update Area contents.
-			time.Sleep(time.Second)
+			time.Sleep(second)
 		}
 		area.Stop()
 	})
@@ -192,7 +192,7 @@ func main() {
 				if ok {
 					field.Println(typeOfS.Field(i).Name)
 				}
-				time.Sleep(time.Millisecond * 250)
+				time.Sleep(second / 4)
 			}
 		}
 	})
